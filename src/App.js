b2429bc55f4ba31db195df16ece273946ec92268
Dan Weaver <danweaver06@gmail.com>
2018-06-05 12:34:26 -0400

    add clarifying commments

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React, { Component } from "react";
 import { graphql, compose } from "react-apollo";
 import gql from "graphql-tag";
 
+// Watch out someone forgot to include the id field of pets.
+// this should normally be required by the server side schema, and you'd get a warning there,
+// but we're not always so lucky.
+// In addition, the actual implementation of the id "getter" can be more complicated with a custom
+// 'dataIdFromObject' implementation
 const personWithPetsWithoutIds = gql`
   query PersonUnderFetching {
     people {
@@ -13,6 +18,8 @@ const personWithPetsWithoutIds = gql`
     }
   }
 `;
+
+// this is the initial query that contains id's
 const personOverview = gql`
   query PersonOverview {
     people {
